3bc849029916595540f3f630c07080a2d20a50ac
ligeance0840 <89499106+ligeance0840@users.noreply.github.com>
2022-05-28 00:26:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="snowy-mountain-84af.outnet.workers.dev";
+        url.hostname="ray01-ligeance001.koyeb.app";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
